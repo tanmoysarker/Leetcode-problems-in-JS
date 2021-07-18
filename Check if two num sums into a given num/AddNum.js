30111bcf52a444b572ec621b16d67a15cefad523
Tanmoy Sarker <tanmoy@Tanmoys-MacBook-Air.local>
2021-07-19 00:15:08 +0600

    array problem start

---
+++ Check if two num sums into a given num/AddNum.js
@@ -0,0 +1,27 @@
+// Given an array of integers, return whether or not two numbers sum to a given target, k.
+// Note: you may not sum a number with itself.
+
+// Ex: Given the following...
+
+// [1, 3, 8, 2], k = 10, return true (8 + 2)
+// [3, 9, 13, 7], k = 8, return false
+// [4, 2, 6, 5, 2], k = 4, return true (2 + 2)
+
+
+function sumFinder(arr, num) {
+    let obj = {}
+    let diff
+  
+    for (let i = 0; i < arr.length; i++) {
+      diff = num - arr[i]
+  
+      if (obj[diff]) {
+        return true
+      } else {
+        obj[arr[i]] = true
+      }
+    }
+    return false
+  }
+  
+  let sumFinderResult = sumFinder([6, 4, 3, 2], 7)
--- Longest Common Prefix/LongestCommonPrefix.js
+++ Longest Common Prefix/LongestCommonPrefix.js
@@ -7,12 +7,14 @@
 // ["a", "b", "c"], return ""
 // ["spot", "spotty", "spotted"], return "spot"
 
+
 function longest_common_starting_substring(arr1){
     var arr= arr1.concat().sort(),
     a1= arr[0], a2= arr[arr.length-1], L= a1.length, i= 0;
     while(i< L && a1.charAt(i)=== a2.charAt(i)) i++;
     return a1.substring(0, i);
     }
+    
 // Another one
     var longestCommonPrefix = function(strs) {
         let prefix = ""
