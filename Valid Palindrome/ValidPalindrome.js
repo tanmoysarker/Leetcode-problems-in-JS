9abdcab9b2a5b4c0dd9a8ce789cc776cc7da2887
Tanmoy Sarker <25175610+tanmoysarker@users.noreply.github.com>
2022-02-26 20:45:51 +0600

    palindrome prolem updated

--- Valid Palindrome/ValidPalindrome.js
+++ Valid Palindrome/ValidPalindrome.js
@@ -19,3 +19,21 @@ function palindrome(str){
         return char === str[str.length() - i - 1];
     })
 }
+
+//Another
+
+function palindrome(str) {
+    var re = /[^A-Za-z0-9]/g; 
+    str = str.toLowerCase().replace(re, '');
+   
+    var len = str.length; 
+    
+    for (var i = 0; i < len/2; i++) {
+      if (str[i] !== str[len - 1 - i]) { 
+          return false; 
+      }
+    }
+    return true; 
+   }
+   
+   palindrome("level");
