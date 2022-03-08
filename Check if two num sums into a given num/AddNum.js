cf88a27daca673e46430830a38d844460bf29665
Tanmoy Sarker <25175610+tanmoysarker@users.noreply.github.com>
2022-03-08 10:31:56 +0600

    hashmap solution added

--- Check if two num sums into a given num/AddNum.js
+++ Check if two num sums into a given num/AddNum.js
@@ -16,12 +16,48 @@ function sumFinder(arr, num) {
       diff = num - arr[i]
   
       if (obj[diff]) {
+        console.log(obj[diff]);
         return true;
       } else {
+        console.log(obj[arr[i]]);
         obj[arr[i]] = true;
       }
     }
     return false
   }
   
-  let sumFinderResult = sumFinder([6, 4, 3, 2], 7)
+  let sumFinderResult = sumFinder([6, 4, 3, 2], 7);
+  console.log(sumFinderResult);
+
+
+  //Another
+
+function twoSum(nums, k) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] == k) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+twoSum([6, 4, 3, 2], 7);
+
+// Hashmap solution
+
+function twoSum(nums, k) {
+  let set = new Map();
+  for(let i = 0; i < nums.length; i++) {
+      let difference = k - nums[i];
+      if(set.has(difference)) {
+          return true;
+      }
+
+      set.set(nums[i]);
+  }
+
+  return false;
+}
+twoSum([6, 4, 4, 2], 7);
