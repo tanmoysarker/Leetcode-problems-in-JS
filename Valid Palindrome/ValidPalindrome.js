654ed696e2cb2f2c27e8d75b73b30ff9ac198e35
Tanmoy Sarker <25175610+tanmoysarker@users.noreply.github.com>
2022-06-09 00:08:25 +0600

    more solutions for palindrome int

--- Valid Palindrome/ValidPalindrome.js
+++ Valid Palindrome/ValidPalindrome.js
@@ -52,3 +52,25 @@ var isPalindrome = function(x) {
         return true;
     }
 };
+
+var isPalindrome = function(x) {
+    if (x < 0) return false
+  
+    let rev = 0
+    for(let i = x; i >= 1; i = Math.floor(i/10)){
+        rev = rev*10 + i%10
+    } 
+    return rev === x
+  };
+
+  var isPalindrome = function(x) {
+    const arr = String(x).split('');
+        
+    while (arr.length > 1) {
+        if (arr.shift() !== arr.pop()) {
+            return false;
+        }
+    }
+    
+    return true;
+};
