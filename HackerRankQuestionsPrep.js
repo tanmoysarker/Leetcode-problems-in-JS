56dacefd215d9e20fd7224b45031eb2078f58be4
Tanmoy Sarker <25175610+tanmoysarker@users.noreply.github.com>
2022-10-18 10:20:01 +0600

    new problem added for hackerrank

--- HackerRankQuestionsPrep.js
+++ HackerRankQuestionsPrep.js
@@ -61,3 +61,37 @@ function repeatedString(s, n){
 
     return a;
 }
+
+// An avid hiker keeps meticulous records of their hikes. During the last hike that took exactly  steps, for every step it was noted if it was an uphill, , or a downhill,  step. Hikes always start and end at sea level, and each step up or down represents a  unit change in altitude. We define the following terms:
+
+// A mountain is a sequence of consecutive steps above sea level, starting with a step up from sea level and ending with a step down to sea level.
+// A valley is a sequence of consecutive steps below sea level, starting with a step down from sea level and ending with a step up to sea level.
+// Given the sequence of up and down steps during a hike, find and print the number of valleys walked through.
+
+// Example
+// Sample Input
+
+// 8
+// UDDDUDUU
+// Sample Output
+
+// 1
+
+ 
+function countingValleys(steps, path) {
+    // Write your code here
+   let valley = 0;
+    let elevation = 0;
+    for(let i = 0; i< steps; i++){
+        if (path[i] === 'D'){
+            elevation--;
+        }else{
+            if(elevation == -1){
+                valley++;
+            }
+            elevation++
+        }
+        
+    }
+    return valley;
+}
