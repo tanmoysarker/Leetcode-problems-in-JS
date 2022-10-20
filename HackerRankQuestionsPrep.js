25c975597787b66dd432c93af205493a9dfc6569
Tanmoy Sarker <25175610+tanmoysarker@users.noreply.github.com>
2022-10-20 23:24:57 +0600

    alternate characters hackerrank problem added

--- HackerRankQuestionsPrep.js
+++ HackerRankQuestionsPrep.js
@@ -130,3 +130,29 @@ function makeAnagram (a, b){
 
     return (2 * diff_of_smaller) + diff_of_larger ;
 }
+
+// You are given a string containing characters  and  only. Your task is to change it into a string such that there are no matching adjacent characters. To do this, you are allowed to delete zero or more characters in the string.
+
+// Your task is to find the minimum number of required deletions.
+
+// s = AABAAB
+// Remove an  A at positions 0 and 3 to make s = ABAB in 2 deletions.
+
+function alternativeCharacters(){
+    let result = 0;
+ 
+    if (s.length >= 1 && s.length <= 100000) {
+         let arr = s.split('');
+         let past = arr[0];
+         
+         arr = arr.filter((item, key) => {
+              if (item !== past && key !== 0) {
+                   past = item;
+                   return item; 
+              }  
+         });
+         result = s.length - [ s.charAt(0), ...arr ].length;
+    }
+    
+    return result; 
+}
