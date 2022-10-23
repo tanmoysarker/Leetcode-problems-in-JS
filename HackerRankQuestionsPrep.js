2eb587715b1d75ff864acb1a419b420b071e8d5a
Tanmoy Sarker <25175610+tanmoysarker@users.noreply.github.com>
2022-10-23 22:15:50 +0600

    Ransom note problem added

--- HackerRankQuestionsPrep.js
+++ HackerRankQuestionsPrep.js
@@ -280,3 +280,23 @@ function substrCount(n, s) {
         }
         return lcs[s1.length][s2.length]
     }
+
+
+//     Harold is a kidnapper who wrote a ransom note, but now he is worried it will be traced back to him through his handwriting. He found a magazine and wants to know if he can cut out whole words from it and use them to create an untraceable replica of his ransom note. The words in his note are case-sensitive and he must use only whole words available in the magazine. He cannot use substrings or concatenation to create the words he needs.
+
+// Given the words in the magazine and the words in the ransom note, print Yes if he can replicate his ransom note exactly using whole words from the magazine; otherwise, print No.
+   
+function checkMagazine(magazine, note) {
+        // Write your code here
+         let result = 'Yes';
+         for (let i = 0; i < note.length; i++){
+             const index = magazine.indexOf(note[i]);
+             if (index === -1){
+                 result =  'No';
+                 break;
+             }else {
+                 magazine.splice(index, 1);
+             }
+         }
+         console.log(result);
+    }
